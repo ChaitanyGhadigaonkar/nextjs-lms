7e446bd72e0e57ddfa7b6b3fed1e5be2e3dc0542
Chaitany Ghadigaonkar <chaitanyghadigaonkar28@gmail.com>
2024-10-31 10:11:24 +0530

    add: bcrypt

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,40 +1,39 @@
 import { AuthOptions, getServerSession } from "next-auth";
 import Github from "next-auth/providers/github";
 import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+import db from "@/db/db";
 
 const authOptions: AuthOptions = {
   providers: [
     Github({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+      profile: (profile, tokens) => {
+        return {
+          id: profile.id,
+          name: profile.display_name,
+          email: profile.email,
+          image: profile.images?.[0]?.url,
+        };
+      },
     }),
     Credentials({
       id: "with-email-password",
       name: "Email and Password",
-      authorize(credentials) {
-        const user = {
-          id: "42",
-          name: credentials?.name,
-          email: "abc@gmail.com",
-          password: "nextauth",
-        };
-        if (
-          credentials?.email === user.email &&
-          credentials?.password === user.password
-        ) {
-          console.log("success");
-          return user;
-        } else {
-          console.log("failed");
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
           return null;
         }
+        const user = {
+          email: credentials.email,
+          password: credentials.password,
+          id: "",
+        };
+        return user;
       },
       credentials: {
-        name: {
-          label: "Name",
-          type: "text",
-          placeholder: "Enter Full Name.",
-        },
         email: {
           label: "Email",
           type: "email",
