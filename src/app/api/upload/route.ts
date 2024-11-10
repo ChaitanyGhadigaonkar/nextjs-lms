dc26fbdc6c6cf7223798f8815f13410f43e0d228
Chaitany Ghadigaonkar <chaitanyghadigaonkar28@gmail.com>
2024-11-10 21:02:22 +0530

    add : cloudinary configuration

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,57 @@
+import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+
+import configureCloudinary from "@/lib/cloudinaryConfig";
+
+// https://stackoverflow.com/questions/75355695/how-to-upload-a-file-to-cloudinary-from-page-server
+async function upload(file: File) {
+  const arrayBuffer = await file.arrayBuffer();
+
+  const buffer = Buffer.from(arrayBuffer);
+
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload_stream({ resource_type: "auto" }, (error, result) => {
+        if (error) {
+          return reject({ success: false, error });
+        }
+        return resolve({
+          success: true,
+          result: {
+            url: result?.url,
+          },
+        });
+      })
+      .end(buffer);
+  });
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData();
+
+    const file = formData.get("file") as File;
+    if (!file) {
+      return NextResponse.json(
+        { success: false, message: "File not found" },
+        { status: 400 }
+      );
+    }
+
+    configureCloudinary();
+    const result = await upload(file);
+
+    return NextResponse.json(
+      {
+        success: true,
+        result: result,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: error, success: false },
+      { status: 400 }
+    );
+  }
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,11 @@
-export { default } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const skipMiddlewareRoutes = ["/api/upload"];
+
+  if (skipMiddlewareRoutes.includes(req.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+}
